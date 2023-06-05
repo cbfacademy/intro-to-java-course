6fb3da430c8e6302e0eeddd13500b7ef19bd9a84
Bea <38406219+builtbybea@users.noreply.github.com>
2023-06-05 23:05:36 +0100

    completed string exercise

--- exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/StringExercises.java
@@ -4,20 +4,51 @@ public class StringExercises {
 
     public String fromCharacters(char[] characters) {
         // TODO - Write code that creates a String from the input array of characters
-        throw new RuntimeException("Not implemented");
+        String newString = new String(characters);
+        System.out.println(newString);
+        return newString;
     }
 
     public Long howMany(String text, Character character) {
         // TODO - Write code to determine how many of the input ${character} are contained in the input ${text}
-        throw new RuntimeException("Not implemented");
+
+        long count = 0;
+    
+        for(int i = 0; i < text.length(); i++) {
+            if(text.charAt(i) == character) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+        return count;
     }
 
     public Boolean isPalindrome(String word) {
         // TODO - Write code to determine whether the input ${word} is a palindrome
-        throw new RuntimeException("Not implemented");
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        if(word.equals(reversedWord)) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public String getName() {
         return "String Exercises";
+    }
+    public static void main( String[] args ) {
+        StringExercises lettersToString = new StringExercises();
+        char individualCharacters[] = {'h', 'i'};
+        char letters[] = {'h', 'e', 'l', 'l', 'o'};
+        lettersToString.fromCharacters(individualCharacters);
+        lettersToString.fromCharacters(letters);
+
+        StringExercises countOccurance = new StringExercises();
+        countOccurance.howMany("hello", 'l');
+
+        StringExercises word = new StringExercises();
+        word.isPalindrome("anna");
     }
 }
