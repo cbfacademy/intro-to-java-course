187f9caf50ebb7243bc8611e85efb787092c2d2a
Genyus <genyus@gmail.com>
2023-09-07 01:13:48 +0100

    feat: implements QuickSort exercise solution

--- exercises/algorithms/src/main/java/com/cbfacademy/App.java
+++ exercises/algorithms/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,28 @@
 package com.cbfacademy;
 
+import com.cbfacademy.sort.QuickSort;
+import com.cbfacademy.sort.ArraySorter;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] arr = { 5, 3, 8, 4, 2, 7, 1, 6 };
+
+        System.out.println("Original array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+
+        ArraySorter sorter = new QuickSort();
+
+        sorter.sort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
     }
 }
