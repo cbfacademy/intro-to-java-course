1feb54cb3acf261924039a008e765c368beb2fa2
Georgina Appeagyei <appeagyeigeorgina@gmail.com>
2024-05-30 21:18:26 +0100

    string format

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -8,7 +8,7 @@ public class App {
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
+        // Write code that concatenates the input parameters and returns the result
         String threeWords = word1.concat(word2).concat(word3);
         // return word1 + " " + word2 + " " + word3
         // return String.join(" ", word1, word2, word3);
@@ -17,12 +17,12 @@ public class App {
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // Write code to determine whether the input parameters are equal strings
+        return word1.equals(word2);
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        throw new RuntimeException("Not implemented");
+        // Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
+        return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
