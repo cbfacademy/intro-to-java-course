e0d591198434030d665faace37182c3872f2047d
Hafsa Kahiye <xhafsaa20@gmail.com>
2024-05-31 17:07:03 +0100

    Updating App.java code

---
+++ exercises/getting-started/getting-started-with-java/com/cbfacademy/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cbfacademy;
+
+public class HelloWorld {
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// Ran javac com/cbfacademy/HelloWorld.java to compile the Java program above & then ran java com.cbfacademy.HelloWorld to run the program
+
--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -6,17 +6,26 @@ public class App {
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
-        throw new RuntimeException("Not implemented");
+        // Write code that concatenates the input parameters and returns the result
+        // throw new RuntimeException("Not implemented");
+        String concatenatedString = word1.concat(word2).concat(word3);
+        // System.out.println(word1.concat(word2).concat(word3));
+        return(concatenatedString);
+
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // Write code to determine whether the input parameters are equal strings
+        // throw new RuntimeException("Not implemented");
+        return word1.equals(word2);
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
-        throw new RuntimeException("Not implemented");
+        // Write code to return a string formatted as follows: "Item: [item]. Price: £[amount]. Quantity: [quantity]". The date should be formatted as DD/MM/YYYY, e.g. 24/10/2023
+        // throw new RuntimeException("Not implemented");
+        String formattedString = String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
+        // You should use %s for strings, %d for integers, and %.2f for doubles (two decimal places for the price).
+        return formattedString; 
+        // return "Item: [item]. Price: £[amount]. Quantity: [quantity]"
     }
 }
