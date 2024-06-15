04b9236a19b98233de3ecc4c1acfeb5cc0574735
Genyus <genyus@gmail.com>
2024-06-15 18:04:28 +0100

    test: adds missing tests and boilerplate

---
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/Search.java
@@ -0,0 +1,12 @@
+package com.cbfacademy.search;
+
+public interface Search {
+    /**
+     * Searches an array.
+     *
+     * @param sortedArray The array to be searched (the array should already be s
+     *                    rted)
+     * @param target      The value to be searched for in the array
+     */
+    int search(int[] sortedArray, int target);
+}
