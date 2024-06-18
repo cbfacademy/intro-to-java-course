3b4eea33b92b3cc91537cfd2f2ac0842d3d46151
Genyus <genyus@gmail.com>
2024-06-18 20:31:21 +0300

    style: fixes comment formatting

--- exercises/algorithms/search/src/main/java/com/cbfacademy/search/Search.java
+++ exercises/algorithms/search/src/main/java/com/cbfacademy/search/Search.java
@@ -4,9 +4,9 @@ public interface Search {
     /**
      * Searches an array.
      *
-     * @param sortedArray The array to be searched (the array should already be s
-     *                    rted)
-     * @param target      The value to be searched for in the array
+     * @param sortedArray The array to be searched (the array must be sorted)
+     * @param target      The value being searched for
+     * @return The array index of the target value, or -1 if the value is not found
      */
     int search(int[] sortedArray, int target);
 }
