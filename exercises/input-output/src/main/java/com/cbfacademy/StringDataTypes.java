69385a038a594990003a2558d716c8c6416da668
Elizabeth Lavender <lizernz24@gmail.com>
2024-08-13 23:31:16 +0100

    String exercise

--- exercises/input-output/src/main/java/com/cbfacademy/StringDataTypes.java
+++ exercises/input-output/src/main/java/com/cbfacademy/StringDataTypes.java
@@ -6,22 +6,11 @@ public class StringDataTypes {
         String one = "word1";
         String two = "word2";
         String three = "word3";
-        String words = one + ""  + two +  ""  + three;
+        String words = one + " "  + two +  " "  + three;
         System.out.println(words);
     
         
     }
     }
 
-    public class BooleanType {
-        public static void main(String[] args) {
-        
-        boolean areEqual = "word1" .equals("word2");
-            
-            System.out.println(areEqual);
-        }
-        }
-        
-        
-        
-        
+   
