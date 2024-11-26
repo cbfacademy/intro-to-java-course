3722a0f6cc85e0a98dc2b6a93fd14dc768d838dc
yogidevbear <andrewsyoga@gmail.com>
2024-11-26 20:44:24 +0000

    Work from Java Primitives session

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -7,16 +7,19 @@ public class App {
 
     public static String concatenate(String word1, String word2, String word3) {
         // TODO: Write code that concatenates the input parameters and returns the result
-        throw new RuntimeException("Not implemented");
+        // throw new RuntimeException("Not implemented");
+        return word1 + word2 + word3;
     }
 
     public static Boolean areEqual(String word1, String word2) {
         // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // throw new RuntimeException("Not implemented");
+        return word1.equals(word2);
     }
 
     public static String format(String item, int quantity, double price) {
         // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[price]. Quantity: [quantity]". The price should be formatted to two decimal places, e.g. 99.99
-        throw new RuntimeException("Not implemented");
+        // throw new RuntimeException("Not implemented");
+        return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
