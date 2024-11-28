5e1841a246ec478f141a83a436f7b226bd6e9e52
Amarachi <amyewulonu@gmail.com>
2024-11-28 20:03:01 +0000

    first commit

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -2,6 +2,11 @@ package com.cbfacademy.cars;
 
 public class App {
     public static void main(String[] args) {
-        // TODO: Display details of all cars in showroom
-    }
+        // Instantiate a Showroom object
+     Showroom showroom = new Showroom();
+
+    // Call getCars() to retrieve the list of cars
+       // Print
+     System.out.println(showroom.getCars());
+}
 }
