cec19be94fee9a86a00c0f07de947fa06fe58fbe
Dannie <daniellehill123@hotmail.co.uk>
2024-11-28 20:39:47 +0000

    oopa part 1 done

--- exercises/java-primitives/src/main/java/com/cbfacademy/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/App.java
@@ -1,7 +1,30 @@
 package com.cbfacademy;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        // Test the methods
+        System.out.println("Hello World!");
+
+        // Example test cases
+        String concatenated = concatenate("Hello", " ", "World!");
+        System.out.println("Concatenated: " + concatenated);
+
+        Boolean equality = areEqual("Java", "java");
+        System.out.println("Are Equal: " + equality);
+
+        String formatted = format("Laptop", 2, 799.99);
+        System.out.println("Formatted: " + formatted);
+    }
+
+    public static String concatenate(String word1, String word2, String word3) {
+        return word1 + word2 + word3;
+    }
+
+    public static Boolean areEqual(String word1, String word2) {
+        return word1.equals(word2);
+    }
+
+    public static String format(String item, int quantity, double price) {
+        return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -1,7 +1,15 @@
 package com.cbfacademy.cars;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        // TODO: Display details of all cars in showroom
+        Showroom showroom = new Showroom();
+
+        List<Car> cars = showroom.getCars();
+
+        for (Car car : cars) {
+            System.out.println(car.getDetails());
+        }
     }
 }
