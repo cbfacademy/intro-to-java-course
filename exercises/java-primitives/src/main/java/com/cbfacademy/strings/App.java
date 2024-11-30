84d6e2088a347106211bc4690f56d1c46b63121b
AtansJP <143465132+AtansJP@users.noreply.github.com>
2024-11-30 00:10:04 +0000

    Update App.java

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -8,7 +8,8 @@ public class App {
         System.out.println(areEqual("Test", "test")); // Output: false
         System.out.println(format("Apple iPhone 15 Pro", 47, 1199.99)); // Output: Item: Apple iPhone 15 Pro. Price: £1199.99. Quantity: 47
     }
- public static String concatenate(String word1, String word2, String word3) {
+
+    public static String concatenate(String word1, String word2, String word3) {
         // Concatenates the input strings and returns the result
         return word1 + word2 + word3;
     }
@@ -19,7 +20,7 @@ public class App {
     }
 
     public static String format(String item, int quantity, double price) {
-        // Formats the string with the specified item, quantity, and price
+        // Formats the string with item, price with £ symbol, and quantity
         return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
