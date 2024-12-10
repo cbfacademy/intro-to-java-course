eaca4d6315c427b21c8cfe6f58c8418a0a47d041
siham455 <siham455@outlook.com>
2024-12-10 16:01:06 +0000

    operation exercise completed

--- exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/operators/App.java
@@ -6,26 +6,26 @@ public class App {
     }
 
     public static double add(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 + operand2;
     }
 
     public static double subtract(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 - operand2;
     }
 
     public static double multiply(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 * operand2;
     }
 
     public static Boolean areEqual(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 == operand2;
     }
 
     public static Boolean isLessThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 < operand2;
     }
 
     public static Boolean isMoreThan(double operand1, double operand2) {
-        throw new RuntimeException("Not implemented");
+        return operand1 > operand2;
     }
 }
