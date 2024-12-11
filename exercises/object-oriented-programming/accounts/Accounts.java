ba5e71c40da2786edbc083670e949061a3031fc1
Amarachi <amyewulonu@gmail.com>
2024-12-11 00:03:03 +0000

    commit all

--- exercises/object-oriented-programming/accounts/Accounts.java
+++ exercises/object-oriented-programming/accounts/Accounts.java
@@ -1,3 +1,36 @@
 public class Accounts {
 
-}
+    // Instance variables
+    private int accountNumber;
+    protected double balance;
+
+    // Constructor
+    public Account(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    // Getter for balance
+    public double getBalance() {
+        return balance;
+    }
+    // Getter for account number
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    // Deposit method
+    public double deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+        return balance;
+    }
+        
+    // Withdrawal method
+    public double withdraw(double requested) {
+        if (requested > 0 && requested <= balance) {
+            balance -= requested;
+        }
+        return 0;
+    }
+    }
+
--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -1,12 +1,16 @@
 package com.cbfacademy.cars;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+
         // Instantiate a Showroom object
      Showroom showroom = new Showroom();
+      List<Car> cars = showroom.getCars();
 
-    // Call getCars() to retrieve the list of cars
-       // Print
-     System.out.println(showroom.getCars());
+    for (Car car : cars) {
+     System.out.println(car);
+    }
 }
 }
