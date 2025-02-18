e10e787e631cab4e9d0595f7179d18cb87deba18
Genyus <genyus@gmail.com>
2025-02-18 12:59:04 +0000

    feat(upstream): adds Software Dev Processes ex/ise
    - Merges from https://github.com/cbfacademy/java-calculator-exercise

---
+++ exercises/software-dev-processes/src/main/java/com/cbfacademy/Calculator.java
@@ -0,0 +1,19 @@
+package com.cbfacademy;
+
+public class Calculator {
+    public static Integer add(final int firstInput, final int secondInput) {
+        return firstInput + secondInput;
+    }
+
+    public static Integer subtract(final int firstInput, final int secondInput) {
+        return firstInput - secondInput;
+    }
+
+    public static Integer multiply(final int firstInput, final int secondInput) {
+        return firstInput * secondInput;
+    }
+
+    public static Integer divide(final int firstInput, final int secondInput) {
+        return firstInput / secondInput;
+    }
+}
